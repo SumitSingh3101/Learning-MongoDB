3f585c8a6a70bd368f4b6b6b593859923ddcc813
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-05-04 00:43:29 +0530

    Learned mongoose with node js

--- index.js
+++ index.js
@@ -17,10 +17,46 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("user", userSchema);
 
-const user1 = new User({
-    name: "Abhishek",
-    email: "john.c.calhoun@examplepetstore.com",
-    age: 20,
+
+User.find({age: {$gt: 47}}).then((res) => {
+    console.log(res[0].name);
+}).catch((err) => {
+    console.log(err);
 });
 
-user1.save();
+
+
+
+
+
+
+
+// User.insertMany([
+//     {name: "Sumit Singh", email: "sumitsingh310106@gmail.com", age: 18},
+//     {name: "Ansh", email: "ansh1307neeraj1978@gmail.com", age: 16},
+//     {name: "Sneaking", email: "sumit1.2.2005singh@gmail.com", age: 18},
+// ]).then(res => {
+//     console.log(res);
+// });
+
+
+// const user1 = new User({
+//     name: "Abhishek",
+//     email: "john.c.calhoun@examplepetstore.com",
+//     age: 20,
+// });
+
+// user1.save();
+
+// const user2 = new User({
+//     name: "Shilu",
+//     email: "jonybhaiya@jpg.com",
+//     age: 69,
+// });
+
+// user2.save().then((res) => {
+//     console.log(res);
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
