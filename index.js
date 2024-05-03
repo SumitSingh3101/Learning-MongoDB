bf7450044263f6ac081bda0039fc7beccce57208
SumitSingh3101 <sumitsingh310106@gmail.com>
2024-05-03 23:50:19 +0530

    Learned mongoose with node js

--- index.js
+++ index.js
@@ -8,3 +8,19 @@ main().then((res) => {
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/test');
 }
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    age: Number,
+});
+
+const User = mongoose.model("user", userSchema);
+
+const user1 = new User({
+    name: "Abhishek",
+    email: "john.c.calhoun@examplepetstore.com",
+    age: 20,
+});
+
+user1.save();
